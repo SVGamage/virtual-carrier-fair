5ee1d1fe9b71d17bfe1eb88850075d615f654ac7
SVGamage <119811762+SVGamage@users.noreply.github.com>
2024-07-22 07:39:25 +0530

    Implement user session handling in layout and header components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,34 @@
-import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import MobileSidebar from "@/components/MobileSidebar";
 import ProfileMenu from "@/components/ProfileMenu";
+type Props = {
+  user: any;
+};
 
-export default function Header() {
+export default function Header({ user }: Props) {
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-      <MobileSidebar />
+      {user !== null && <MobileSidebar />}
       <div className="w-full flex-1">
-        <h1 className="block font-bold text-blue-600 text-md md:hidden ">
+        <h1
+          className={`block font-bold text-blue-600 text-md ${user !== null && "md:hidden"}`}
+        >
           Virtual Carrier Fair
         </h1>
       </div>
       <nav className="flex items-center justify-center gap-2">
-        <LoginLink className="bg-blue-600 py-1 px-3.5  text-white font-bold border-solid border-2 border-blue-600 rounded-3xl">
-          Login
-        </LoginLink>
-        <ProfileMenu />
+        {user === null ? (
+          <LoginLink className="bg-blue-600 py-1 px-3.5  text-white font-bold border-solid border-2 border-blue-600 rounded-3xl">
+            Login
+          </LoginLink>
+        ) : (
+          <>
+            <LogoutLink className="bg-blue-600 py-1 px-3.5  text-white font-bold border-solid border-2 border-blue-600 rounded-3xl">
+              Logout
+            </LogoutLink>
+            <ProfileMenu />
+          </>
+        )}
       </nav>
     </header>
   );
